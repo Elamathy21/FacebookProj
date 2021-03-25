5b9b2021b732a78e3800ba91113608805c1ad91d
Elamathy21 <elamathyforever@gmail.com>
2021-03-25 08:51:26 +0530

    b push

--- src/com/sample/SimpleProg.java
+++ src/com/sample/SimpleProg.java
@@ -6,5 +6,9 @@ public class SimpleProg {
 		int a = 10, b=10;
 		int c = a+b;
 		System.out.println(c);
+		
+		int e = a*b;
+		System.out.println(e);
+		System.out.println("Product performed");
 	}
 }
